82cdffa1ab9b22cbdad3b040386c25f480fd4af2
David Pattison <dspattison@gmail.com>
2012-04-18 22:42:54 -0700

    got the game mechanics working

--- app/mailers/tte/turn_mailer.rb
+++ app/mailers/tte/turn_mailer.rb
@@ -4,8 +4,20 @@ class Tte::TurnMailer < ActionMailer::Base
   def turn_notify tte_game, last_turn
     
     @board = Tte::Board.new last_turn.board
+    @tte_game = tte_game
+    @last_turn = last_turn
+    if last_turn.number % 2 == 1
+      @other_player_name = tte_game.player_a_email
+      @current_player = Tte::Board::TILE_X
+      @current_player_name = tte_game.player_b_email
+    else
+      @other_player_name = tte_game.player_b_email
+      @current_player = Tte::Board::TILE_O
+      @current_player_name = tte_game.player_a_email
+    end
     
-    mail(:to => 'dspattison@gmail.com') do |format|
+    
+    mail(:to => @current_player_name) do |format|
        format.html
     end
   end
